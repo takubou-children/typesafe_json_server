9ac6ad697f97bf3133f3ad30bad9bea775cc6407
taku10101 <taku1011ht@gmail.com>
2024-12-28 01:48:25 +0900

    add yamada ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { UIProvider } from "@yamada-ui/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,9 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <UIProvider>{children}</UIProvider>
       </body>
     </html>
   );
